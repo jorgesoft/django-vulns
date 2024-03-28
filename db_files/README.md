8e5fa891254a2b0f760da18925bd102c4570627e
jorgesoft <jorgecarrilloit@gmail.com>
2024-03-28 04:15:21 -0400

    docker compose connects django and mysql

--- db_files/README.md
+++ db_files/README.md
@@ -1,4 +1,8 @@
+cd db_files
 docker build -t vulns-mysql .
+cd ..
+docker-compose up --build --force-recreate
+
 
 mysql -h 127.0.0.1 -P 3306 -u root -p
 
--- django_vulns/settings.py
+++ django_vulns/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_vulns.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'mypassword',
+        'HOST': 'mysql',  # Use 'localhost' if your database is not running in Docker
+        'PORT': '3306',
     }
 }
 
