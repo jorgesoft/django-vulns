96427be401763e4e48df19aaa1d1d2d2fa50b422
jorgesoft <jorgecarrilloit@gmail.com>
2024-03-31 00:36:08 -0400

    django changed list function to raw query

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,6 +6,8 @@ from .forms import VulnerabilitiesForm
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView, UpdateView
 from django.shortcuts import get_object_or_404
+from django.db import connection
+from django.contrib import messages
 
 class HomeView(TemplateView):
     template_name = 'dashboard/home.html'
@@ -24,10 +26,23 @@ def groups(request):
 
 def vulnerabilities_list(request):
     search_query = request.GET.get('cve_search', '')
-    if search_query:
-        vulnerabilities = Vulnerabilities.objects.filter(cve__icontains=search_query)
-    else:
-        vulnerabilities = Vulnerabilities.objects.all()
+    vulnerabilities = []  # Initialize as an empty list
+
+    with connection.cursor() as cursor:
+        if search_query:
+            # Use ILIKE for case-insensitive matching in PostgreSQL; use LIKE for other databases
+            cursor.execute("SELECT * FROM vulnerabilities WHERE cve LIKE %s", ['%' + search_query + '%'])
+        else:
+            cursor.execute("SELECT * FROM vulnerabilities")
+        result = cursor.fetchall()
+        
+        if result:  # Ensure result is not None
+            columns = [col[0] for col in cursor.description]
+            vulnerabilities = [
+                dict(zip(columns, row))
+                for row in result
+            ]
+
     return render(request, 'dashboard/vulnerabilities_list.html', {'vulnerabilities': vulnerabilities, 'search_query': search_query})
 
 def create_vulnerability(request):
