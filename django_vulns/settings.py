efb8616891f0873128d2fe27d88deaec458c518e
jorgesoft <jorgecarrilloit@gmail.com>
2024-03-31 01:10:08 -0400

    django added csrf settings

--- django_vulns/settings.py
+++ django_vulns/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSFR settings
+SESSION_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = ['https://db.nubitlan.com']
