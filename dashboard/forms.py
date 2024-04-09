78a92842e39e565ab26f7bc91000cd43407d7dd1
Jorge Silva <jorgecarrilloit@gmail.com>
2024-04-09 02:15:52 -0400

    django added host form

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Vulnerabilities
+from .models import Vulnerabilities, Hosts, Os
 
 class VulnerabilitiesForm(forms.Form):
     # The same fields as the Vulnerabilities table
@@ -8,3 +8,20 @@ class VulnerabilitiesForm(forms.Form):
     description = forms.CharField(max_length=255)
     severity = forms.DecimalField(max_digits=5, decimal_places=2)
     cwe = forms.CharField(max_length=100)
+
+
+class HostForm(forms.ModelForm):
+    os_id = forms.ModelChoiceField(
+        queryset=Os.objects.all(),
+        label="Operating System",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        to_field_name="id",  # Ensures the ID is used
+    )
+
+    class Meta:
+        model = Hosts
+        fields = ['name', 'ip', 'os_id']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Custom label presentation can be handled here if needed
