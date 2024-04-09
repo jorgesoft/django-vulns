36c9c012f93719b6d60a670199eb6d94e113d652
Jorge Silva <jorgecarrilloit@gmail.com>
2024-04-09 02:19:30 -0400

    django added urls and templates for hosts views

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,13 +4,25 @@ from .views.vulnerabilities import (
     vulnerabilities_list, 
     create_vulnerability, 
     delete_vulnerability, 
-    update_vulnerability
+    update_vulnerability,
+)
+from .views.hosts import (
+    hosts_list,
+    create_host
+    #delete_host,
+    #update_host,
 )
 
 urlpatterns = [
+    # Vulnerabilities URLs
     path('', HomeView.as_view(), name='home'),
     path('vulnerabilities/', vulnerabilities_list, name='vulnerabilities'),
     path('vulnerabilities/create/', create_vulnerability, name='vulnerability-create'),
     path('vulnerabilities/delete/<str:cve>/', delete_vulnerability, name='vulnerability-delete'),
     path('vulnerability/update/<str:cve>/', update_vulnerability, name='vulnerability-update'),
+    # Host URLs
+    path('hosts/', hosts_list, name='hosts'),
+    path('hosts/create/', create_host, name='host-create'),
+    #path('hosts/delete/<int:host_id>/', delete_host, name='host-delete'),
+    #path('host/update/<int:host_id>/', update_host, name='host-update'),
 ]
