3dba70f935150faa3ab643463821a876816f6a0c
Jorge Silva <jorgecarrilloit@gmail.com>
2024-04-21 16:46:16 -0400

    graph.js edited graph styling and made minor changes to html for better display and organization

--- dashboard/static/js/charts-setup.js
+++ dashboard/static/js/charts-setup.js
@@ -9,32 +9,65 @@ function setupCharts(vulnerabilityData, hostData, host_vulns_data) {
                 data: vulnerabilityData,
                 backgroundColor: ['#007bff', '#4bbf73', '#f0ad4e', '#fd7e14', '#d9534f']
             }]
-        }
+        },
+        options: {
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: true,
+                text: 'Severity rating for all found vulnerabilities.'
+              }
+            }
+          },
     });
 
     // Setup Host Chart
     var ctxHostChart = document.getElementById('hostChart').getContext('2d');
     new Chart(ctxHostChart, {
-        type: 'pie',
+        type: 'doughnut',
         data: {
             labels: hostData.labels,
             datasets: [{
                 data: hostData.data,
                 backgroundColor: ['#007bff', '#6610f2', '#6f42c1', '#4bbf73', '#20c997']
             }]
-        }
+        },
+        options: {
+            plugins: {
+              legend: {
+                position: 'right',
+              },
+              title: {
+                display: true,
+                text: 'Count of hosts based on their OS.'
+              }
+            }
+          },
     });
 
     // Setup Software Chart
     var ctxSoftwareChart = document.getElementById('hostVulnsChart').getContext('2d');
     new Chart(ctxSoftwareChart, {
-        type: 'pie',
+        type: 'doughnut',
         data: {
             labels: host_vulns_data.labels,
             datasets: [{
                 data: host_vulns_data.data,
                 backgroundColor: ['#007bff', '#6610f2', '#6f42c1', '#4bbf73', '#20c997']
             }]
-        }
+        },
+        options: {
+            plugins: {
+              legend: {
+                position: 'right',
+              },
+              title: {
+                display: true,
+                text: 'Count of vulnerabilities found by host.'
+              }
+            }
+          },
     });
 }
